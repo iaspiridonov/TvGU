61577177bcbb8b940a12cab7b4b9eb6ef8a3c85d
Ivan Martynenko <iwan@example.com>
2021-08-26 07:01:49 +0300

    done

---
+++ src/js/file.js
@@ -0,0 +1,175 @@
+import $ from 'jquery';
+
+global.jQuery = $;
+global.$ = $;
+
+import Swiper from 'swiper';
+
+import Masonry from 'masonry-layout';
+
+import Mmenu from 'mmenu-js';
+
+
+$(document).ready(function () {
+
+  var msnry = new Masonry('.news', {
+    itemSelector: '.news__item'
+  });
+
+
+  let header = $('.header');
+  let navItem = $('.nav-menu__item');
+  let navDropWrapper = $('.nav-menu__drop-wrapper')
+
+  navItem.click(function () {
+    navDrop = $('#js-nav-drop');
+    navTitle = $(this).find('.nav-menu__link').text();
+    $('.nav-menu__drop-title').html('');
+    navDrop.find('.nav-menu__drop').html('');
+
+    if ($(this).hasClass('nav-menu__item--active')) {
+      navItem.removeClass('nav-menu__item--active');
+      $(this).removeClass('nav-menu__item--active');
+      header.removeClass('header--active');
+      navDropWrapper.removeClass('nav-menu__drop-wrapper--active');
+      $('.nav-menu__drop-title').html('');
+      navDrop.find('.nav-menu__drop').html('');
+
+    } else {
+      navItem.removeClass('nav-menu__item--active');
+      $(this).addClass('nav-menu__item--active');
+      header.addClass('header--active');
+      navDropWrapper.addClass('nav-menu__drop-wrapper--active');
+
+      $('.nav-menu__drop-title').text(navTitle);
+      navDropItems = $(this).find('.nav-menu__drop-item').clone();
+      $.each(navDropItems, function (index, val) {
+        navDrop.find('.nav-menu__drop').append(val);
+      });
+
+    }
+  });
+
+  $(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+      $('.header--inner').removeClass('header--transparent');
+    } else {
+      $('.header--inner').addClass('header--transparent');
+    }
+  });
+
+  var swiperMainLikns = new Swiper(".js-swiper--main-links", {
+    slidesPerView: 5,
+    spaceBetween: 48,
+    scrollbar: {
+      el: ".swiper-scrollbar--main-links",
+      draggable: true,
+      snapOnRelease: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next--main-links",
+      prevEl: ".swiper-button-prev--main-links",
+    },
+    mousewheel: {
+      forceToAxis: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      440: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1600: {
+        slidesPerView: 5,
+        spaceBetween: 48,
+      },
+    },
+  });
+
+  var swiperMainLikns = new Swiper(".js-swiper--usefull-links", {
+    slidesPerView: 5,
+    spaceBetween: 48,
+    scrollbar: {
+      el: ".swiper-scrollbar--usefull-links",
+      draggable: true,
+      snapOnRelease: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next--usefull-links",
+      prevEl: ".swiper-button-prev--usefull-links",
+    },
+    mousewheel: {
+      forceToAxis: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      440: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1600: {
+        slidesPerView: 5,
+        spaceBetween: 48,
+      },
+    },
+  });
+
+  $(document).click(function (e) {
+    let header = $('.header');
+    let navDropWrapper = $('.nav-menu__drop-wrapper')
+    let navItem = $('.nav-menu__item');
+    if (!header.is(e.target) && header.has(e.target).length === 0) {
+      header.removeClass('header--active');
+      let navDropWrapper = $('.nav-menu__drop-wrapper--active');
+      $('.nav-menu__drop-title').html('');
+      $('#js-nav-drop').find('.nav-menu__drop').html('');
+      navItem.removeClass('nav-menu__item--active');
+      navDropWrapper.removeClass('nav-menu__drop-wrapper--active');
+    };
+  });
+
+  const menu = new Mmenu("#js-main-menu", {
+    "navbars": [
+      {
+        "position": "bottom",
+        "content": [
+          "<div class='header__phone header__phone--mmenu'><a href='tel:+748220000000'>+7 4822 000 00 00</a><span>Приемная</span></div>"
+        ]
+      },
+      {
+        "position": "bottom",
+        "content": [
+          "<a href='#'><img src='/images/icon/rus.svg' class='mr-2' alt='RUS'>ENG</a>"
+        ]
+      }
+    ],
+    extensions: [
+      "pagedim-black",
+    ],
+    navbar: {
+      title: "<img src='/images/logo_with_text.svg' alt='logo'>"
+    }
+  },
+    {
+      offCanvas: {
+        clone: true
+      }
+    });
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,176 @@
+/**
+ * Webpack main configuration file
+ */
+
+const path = require('path');
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { extendDefaultPlugins } = require('svgo');
+const webpack = require("webpack");
+
+const environment = require('./configuration/environment');
+
+const templateFiles = fs.readdirSync(environment.paths.source)
+  .filter((file) => path.extname(file).toLowerCase() === '.html');
+
+const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
+  inject: true,
+  hash: false,
+  filename: template,
+  template: path.resolve(environment.paths.source, template),
+  favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
+}));
+
+module.exports = {
+  entry: {
+    app: path.resolve(environment.paths.source, 'js', 'app.js'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: environment.paths.output,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'images/icon/[name].[hash:6].[ext]',
+              publicPath: '../'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'images/[name].[hash:6].[ext]',
+              publicPath: '../'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'images/content/[name].[hash:6].[ext]',
+              publicPath: '../'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [MiniCssExtractPlugin.loader, { loader: 'css-loader', options: { url: false, sourceMap: true } }, 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'fonts/[name].[hash:6].[ext]',
+              publicPath: '../',
+              limit: environment.limits.fonts,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery',
+      "window.jQuery": 'jquery',
+      "window.jquery": 'jquery',
+      'window.$': 'jquery',
+      Popper: ['popper.js', 'default']
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new ImageMinimizerPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: extendDefaultPlugins([
+                {
+                  name: 'removeViewBox',
+                  active: false,
+                },
+              ]),
+            },
+          ],
+        ],
+      },
+    }),
+    new CleanWebpackPlugin({
+      verbose: true,
+      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'images', 'content'),
+          to: path.resolve(environment.paths.output, 'images', 'content'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'images', ''),
+          to: path.resolve(environment.paths.output, 'images', ''),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'images', 'icon'),
+          to: path.resolve(environment.paths.output, 'images', 'icon'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+  ].concat(htmlPluginEntries),
+  target: 'web',
+};
